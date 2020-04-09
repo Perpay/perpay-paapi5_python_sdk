b9330fa3f4195e19a421d1494d7d330603a983c2
Jacky Lee <jacky@perpay.com>
2020-04-09 14:21:41 -0400

    Changed name of setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='perpay-paapi5_python_sdk',
+    name='paapi5_python_sdk',
     version='1.0.0',
     description='Private package created by Amazon hosted by Perpay',
     url='git@github.com:Perpay/perpay-paapi5_python_sdk.git',
