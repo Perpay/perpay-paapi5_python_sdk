93eb1d4db910cd047331163f04350c44309f17e5
Jacky Lee <jacky@perpay.com>
2020-04-09 13:59:42 -0400

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='perpay-paapi5_python_sdk',
+    version='1.0.0',
+    description='Private package created by Amazon hosted by Perpay',
+    url='git@github.com:Perpay/perpay-paapi5_python_sdk.git',
+    author='Jacky Lee',
+    author_email='jacky@perpay.com',
+    license='unlicense',
+    packages=['perpay'],
+    zip_safe=False
+)
